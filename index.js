cd9c8cc9f02ee9923004781feb7bb59fcab39f48
Okechukwu Uneze <unezeo@gmail.com>
2026-05-07 14:21:20 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+calculateTax = (amount) => {
+    return amount * 0.10;
+}
 
+convertToUpperCase = (text) => {
+    return text.toUpperCase();
+}
 
+findMaximum = (num1, num2) => {
+    return Math.max(num1, num2);
+}
 
+isPalindrome = (word) => {
+    const cleaned = word.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const reversed = cleaned.split('').reverse().join('');
+    return cleaned === reversed;
+}
 
+calculateDiscountedPrice = (originalPrice, discountPercentage) => {
+    const discountAmount = originalPrice * (discountPercentage / 100);
+    return originalPrice - discountAmount;
+}
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
